b3b8aae551b3495baa8cfcd4fc1919c4c7f463ef
Pablo Aliaga <pablooaliaga@gmail.com>
2019-07-06 15:04:16 -0400

    Se suben las tareas y se eliminan los archivos caché de drRacket

--- Lenguajes de progamación/Tarea3/main.rkt
+++ Lenguajes de progamación/Tarea3/main.rkt
@@ -1,5 +1,8 @@
 #lang play
 
+;Author: Pablo Aliaga
+;implementación lenguaje orientado a objetos utilizando Scheme como base
+
 #|
 <expr> ::= <num>
          | <id>
@@ -52,7 +55,10 @@
   (get obj id)
   (set obj id val)
   (send obj id values)
-  (this)
+  (call obj id values) ; se usa luego de que se realiza un send
+  (this) ; reserved words, lookup in the object class ; also refered to the object
+  (super) ; reserved words, lookup in the parent class of method ; also refered to the object
+  (fun) ; reserved words
   )
 
 ;;member
@@ -60,13 +66,17 @@
   (field id expr)
   (method id ins body))
 
+;;method
+(deftype Application
+  (methodV ins body)) ;;este value es util para guardar los métodos en el env
+; sin embargo no son valores como tal, no se pueden retornar, pero se usan en el send
+
 ;; values
 (deftype Val
   (numV n)
   (boolV b)
-  (classV parent fields methods env) ;;class-env can have methods and fields
+  (classV parent fields methods env) ;;class-env can have methods and fields, also has the env in which was created
   (objectV class values) ;;object-env only doesn't have methods, only fields
-  (methodV ins body)
   (None))
 
 (deftype Def
@@ -107,24 +117,18 @@ Busca un símbolo en el ambiente, retornando su valor asociado.
          (env-lookup x rest))]))
 
 
-
-;;method-lookup datatype,
-;;básicamente entrega el metodo encontrado y su posición relativa en los frames de metodos
-(deftype MethodResponse
-  (method-response m p)
-  )
-
 #|
 method-lookup:: Sym Env -> MethodV
 Busca un símbolo en el ambiente de métodos, retornando su método asociado.
+En caso de no encontrarlo lanza un #f para que el interprete busque en la clase padre de la actual
 |#
-(define (method-lookup x env [counter 0])
+(define (method-lookup x env)
   (match env
     [(mtEnv) (error 'method-lookup "Object class don't have method: ~a" x)]
     [(aEnv hash rest)
      (if (hash-has-key? hash x)
-         (method-response (hash-ref hash x) counter)
-         (method-lookup x rest (add1 counter)))]))
+         (hash-ref hash x)
+         #f)]))
 
 #|
 multi-extend-env:: List(Sym) List(Expr) Env -> Env
@@ -150,19 +154,31 @@ Este método no crea un nuevo ambiente.
 #|
 update-frame-env!:: Sym Val Env -> Void
 Actualiza un hash-value del ambiente usando mutación
-Este método se usara para los setters de los objetos
+Este método se usara para los setters de los objetos;
+Extend-frame-env NO actualiza keys que ya existan, por esto fue creado esta función
 |#
 (define (update-frame-env! id val env)
   (match env
     [(mtEnv) (aEnv (make-hash (list (cons id val))) env)]
     [(aEnv h rEnv) (hash-set! h id val)]))
 
+#|
+copy-env:: Env -> Env
+Crea una nueva estructura de aEnv que es copia de el env entregado, pero que no comparte direcciones de memoria,
+ es decir, no se afecta por la mutación de su copia
+|#
+(define (copy-env env)
+  (match env
+    [(mtEnv) (mtEnv)]
+    [(aEnv hash rest) (aEnv (hash-copy hash) (copy-env rest))])
+  )
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; parse :: s-expr -> Expr
 (define (parse s-expr)
   (match s-expr
     ['this (this)]
+    ['super (super)]
     [(? number?) (num s-expr)]
     [(? symbol?) (id s-expr)]    
     [(? boolean?) (bool s-expr)]
@@ -191,6 +207,10 @@ Este método se usara para los setters de los objetos
     [(list 'get o id) (get (parse o) (parse id))]
     [(list 'set o id val) (set (parse o) (parse id) (parse val))]
     [(list 'send o m vals ...) (send (parse o) (parse m) (map parse vals))]
+    [(list 'super m vals ...) (send (super) (parse m) (map parse vals))] ; super call
+    [(list 'fun (list ins ...) body) (new
+                                      (class Object (list (method (id (fun)) (map parse ins) (parse body)))))] ; lambda implementation with objects
+    [(list idFun vals ...) (send (parse idFun) (id (fun)) (map parse vals))] ; lambda aplication
     ))
 
 ;; Root Object
@@ -203,6 +223,8 @@ Este método se usara para los setters de los objetos
     [(list 'define id b) (my-def id (parse b))]))
 
 ;; interp :: Expr Env -> Val
+;; Este interprete se usa cuando NO estamos interpretando un método de un objeto,
+;; por eso luego de interpretar un send, se usa interp-obj en vez de este
 (define (interp expr env)
   (match expr
     [(num n) (numV n)]    
@@ -230,7 +252,8 @@ Este método se usara para los setters de los objetos
     
     ;; class related stuffs!
     
-    [(this) (env-lookup (this) env)]
+    [(this) (error "This is a reserved word for objects")]
+    [(super) (error "Super is a reserved word for objects")]
     [(classV parent fields methods class-env) expr] ; Already interpreted classV
     [(objectV class values) expr] ;Already interpreted objectV
     [(class parent members) (let* ([fields (filter field? members)]
@@ -243,29 +266,111 @@ Este método se usara para los setters de los objetos
                                    [field-env (multi-extend-env id-fields values-fields (classV-fields interp-parent))]
                                    [method-env (multi-extend-env id-methods values-methods (classV-methods interp-parent))])
                               (classV interp-parent field-env method-env env))]
-    
     [(send o m args) (let* ([obj (interp o env)]
-                            [methodR (method-lookup (id-s m) (classV-methods (objectV-class obj)))]
-                            [m (method-response-m methodR)] ;; método en sí
-                            [p (method-response-p methodR)] ;; posición de la clase a la que pertenece el método
-                            [id-args (map id-s (methodV-ins m))] ;;agregar args al ambiente
-                            [values-args (map (λ (arg) (interp arg env)) args)]
-                            [new-env (multi-extend-env id-args values-args env)])
-                       
-                       (begin
-                         (extend-frame-env! (this) obj new-env) ;;agregar this al ambiente
-                         (interp (methodV-body m) new-env))
-                       )]
+                            [new-expr (call obj m args)]
+                            [fields (objectV-values obj)]
+                            [methods (classV-methods (objectV-class obj))])
+                        (interp-obj new-expr obj fields methods env)) ;; inicializa el llamado dentro de un objeto
+                       ]
                      
                            
     [(new c) (let ([origin-class (interp c env)])
-               (objectV origin-class (classV-fields origin-class)))]
-    [(get o id) (let ([obj (interp o env)])
-                    (interp id (objectV-values obj))
-                    )]
-    [(set o id val) (let ([obj (interp o env)])
-                        (update-frame-env! (id-s id) (interp val env) (objectV-values obj))
-                      )]
+               (objectV origin-class (copy-env (classV-fields origin-class))))]
+    [(get o id) (let* ([obj (interp o env)]
+                       [fields (objectV-values obj)]
+                       [methods (classV-methods (objectV-class obj))])
+                  (interp-obj (get obj id) obj fields methods env))]
+    [(set o id val) (let* ([obj (interp o env)]
+                       [fields (objectV-values obj)]
+                       [methods (classV-methods (objectV-class obj))])
+                      (interp-obj (set obj id val) obj fields methods env))]
+    ))
+
+
+#|
+interp-object :: Expr ObjectV Env[Fields] Env[Methods] Env -> Val
+Interprete exclusivo para metodos o llamados a objetos. Se utiliza luego de un llamado send.
+Rste mantiene en sus argumentos siempre los fields y metodos del objeto y la clase respectivamente,
+de esta forma se puede manejar de manera sencilla , los llamados a this, la busqueda de fields con sus métodos respectivos (respetando field shadowing)
+y los llamados a super.
+|#
+(define (interp-obj expr this-obj fields methods env)
+  (match expr
+    [(num n) (numV n)]    
+    [(bool b) (boolV b)]    
+    [(binop f l r) (make-val (f (open-val (interp-obj l this-obj fields methods env))
+                                (open-val (interp-obj r this-obj fields methods env))))]
+    [(unop f s) (make-val (f (open-val (interp-obj s this-obj fields methods env))))]
+    [(my-if c t f)
+     (def (boolV cnd) (interp-obj c this-obj fields methods env))
+     (if cnd
+         (interp-obj t this-obj fields methods env)
+         (interp-obj f this-obj fields methods env))]
+    [(id x) (env-lookup x env)]        
+    [(seqn expr1 expr2) (begin 
+                          (interp-obj expr1 this-obj fields methods env)
+                          (interp-obj expr2 this-obj fields methods env))]
+    [(lcal defs body)
+     (let* ([new-env (multi-extend-env '() '() env)])
+       (for-each (λ(x)
+                   (let ([in-def (interp-def-obj x this-obj fields methods new-env)])
+                     (extend-frame-env! (car in-def) (cdr in-def) new-env)
+                     #t)) defs)       
+       (interp-obj body this-obj fields methods new-env))     
+     ]
+    
+    ;; class related stuffs!
+    
+    [(this) this-obj]
+    [(super) this-obj]
+    [(classV parent fields methods class-env) expr] ; Already interpreted classV
+    [(objectV class values) expr] ;Already interpreted objectV
+    [(class parent members) (let* ([fields (filter field? members)] ;se filtran los members que sean fields
+                                   [methods (filter method? members)] ; se filtran los members que sean methods
+                                   [id-fields (map (λ (v) (id-s (field-id v))) fields)]
+                                   [values-fields (map (λ (v) (interp-class-member-obj v this-obj fields methods env)) fields)]
+                                   [id-methods (map (λ (v) (id-s (method-id v))) methods)]
+                                   [values-methods (map (λ (v) (interp-class-member-obj v this-obj fields methods env)) methods)]
+                                   [parent (interp-obj parent this-obj fields methods env)] ; se interpreta parent
+                                   [field-env (multi-extend-env id-fields values-fields (classV-fields parent))]
+                                   ; se crea el Env[Field] de la clase, este es un (aEnv hash env), 
+                                   ; que crea un hash con los fields nuevos y guarda el field-env del padre
+                                   [method-env (multi-extend-env id-methods values-methods (classV-methods parent))])
+                                   ; se crea el Env[Method] de la clase, este es un (aEnv hash env), 
+                                   ; que crea un hash con los methods nuevos y guarda el method-env del padre
+                              (classV parent field-env method-env env))]
+
+    [(send o m args) (let* ([obj (interp-obj o this-obj fields methods env)]
+                            [new-expr (call obj m args)]
+                            [new-fields (objectV-values obj)]
+                            [new-methods (classV-methods (objectV-class obj))])
+                        (if (super? o)
+                            (interp-obj new-expr obj (aEnv-env fields) (aEnv-env methods) env) ;; caso de llamado a super
+                            (interp-obj new-expr obj new-fields new-methods env)) ;; inicializa el llamado dentro de un objeto
+                            )]
+                        
+
+      ;; Es en call donde se busca por "Capas" los metodos del objeto, si no se encuentra el metodo en la capa de metodos actuales
+      ;; se busca en la siguiente, y con ello también se pasa a la siguiente capa de fields (field shadowing)
+    [(call o m args) (let* ([obj (interp-obj o this-obj fields methods env)]
+                            [methodR (method-lookup (id-s m) methods)])
+                       (if methodR
+                           (let* ([id-args (map id-s (methodV-ins methodR))] ;;agregar args al ambiente
+                                  [values-args (map (λ (arg) (interp-obj arg this-obj fields methods env)) args)]
+                                  [new-env (multi-extend-env id-args values-args env)])
+                             (begin
+                               (extend-frame-env! (this) obj new-env) ;;agregar this al ambiente
+                               (interp-obj (methodV-body methodR) this-obj fields methods new-env)))
+                           
+                           (interp-obj (call o m args) this-obj (aEnv-env fields) (aEnv-env methods) env) ;; Caso en que no encuentre el método
+                           ))]
+                     
+                           
+    [(new c) (let ([origin-class (interp-obj c this-obj fields methods env)])
+               (objectV origin-class (copy-env (classV-fields origin-class))))] ;; Aca es importante que se usa copy-env, para que dos objetos de una misma clase
+    [(get o id) (interp-obj id this-obj fields methods fields)]                 ;; no compartan sus values
+    [(set o id val)
+     (update-frame-env! (id-s id) (interp-obj val this-obj fields methods env) fields)]
     ))
   
 
@@ -297,10 +402,25 @@ Este método se usara para los setters de los objetos
   (match a-def
     [(my-def id body) (cons id (interp body env))]))
 
+;; interp-def-obj :: Def, Env -> Expr
+;; interp-def para el caso en que estemos dentro del cuerpo de un objeto
+(define (interp-def-obj a-def this fields methods env)
+  (match a-def
+    [(my-def id body) (cons id (interp-obj body this fields methods env))]))
+
+;; interp-class-members-obj :: member -> Value
+;; Toma un member y lo deja como valores , en el caso de los metodos como methodV
+;; caso en el que estemos interpretando dentro de los métodos de un objeto
+(define (interp-class-member-obj member this fields methods env)
+  (match member
+    [(field id val) (interp-obj val this fields methods env)]
+    [(method id ins body) (methodV ins body)]
+    [else (error "not valid member of class")] ))
+
 ;; run :: s-expr -> Val
 (define (run s-expr)
   (interp (parse s-expr) empty-env))
-3
+
 #|
 run-val:: s-expr -> Scheme-Val + Val
 Versión alternativa de run, que retorna valores de scheme para primitivas y
@@ -311,4 +431,6 @@ valores de MiniScheme para clases y objetos
   (match val
     [(numV n) n]
     [(boolV b) b]
+    [(classV parent fields methods env) "<Class>"] ;Esto es para poder mostrar las clases al retornarlas como valor
+    [(objectV class values) "<Object>"] ;Esto es para poder mostrar los objetos al retornarlos como valor
     [x x]))
--- Lenguajes de progamación/Tarea3/tests.rkt
+++ Lenguajes de progamación/Tarea3/tests.rkt
@@ -2,6 +2,9 @@
 (require "main.rkt")
 (print-only-errors)
 
+;Author: Pablo Aliaga
+;implementación lenguaje orientado a objetos utilizando Scheme como base
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;                                 TESTS BASE                                  ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -23,6 +26,16 @@
 ;                                  SUS TESTS                                  ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;Objetos y clases como valores (primer orden)
+(test (run-val '(local
+                  [(define A (class))]
+                  A)) "<Class>")
+
+(test (run-val '(local
+                  [(define A (class))]
+                  (new A))) "<Object>")
+
+;Objects and Classes respect lexical scoping
 (test/exn (run-val '(seqn (local
                   [(define A
                      (class
@@ -32,14 +45,29 @@
                 (new A)))
 "env-lookup: free identifier: A")
 
+;This y super solo se pueden usar dentro de los métodos de un objeto
+(test/exn (run-val '(+ this 2)) "This is a reserved word for objects")
+(test/exn (run-val '(+ super 2)) "Super is a reserved word for objects")
+
+
+
+;Clases y objetos, básicos
+
+;Tests de metodo que no existe en objeto
+(test/exn (run-val '(local
+                  [(define A (class)) (define a (new A))]
+                  (send a e))) "Object class don't have method:")
+(test/exn (run-val '(local
+                  [(define A (class (method x () 0))) (define a (new A))]
+                  (send a e))) "Object class don't have method:")
+
+
+;Tests con objetos de prueba, con metodos como suma o otro básicos
 (test (run-val '(local
             [(define x 10)
              (define A
-               (class
-                   (method m (y) (+ x y))))
-             (define o (new A))]
-            (send o m 1)))
-11)
+               (class (method m (y) (+ x y))))
+             (define o (new A))] (send o m 1))) 11)
 
 (test (run-val '(local
              [(define c (class
@@ -52,8 +80,6 @@
               (send o set-x (+ 1 3))
               (+ (send o sum 3) (get o y)))))
 11)
-
-
 (test (run-val '(local
               [(define A
                  (class
@@ -65,6 +91,49 @@
                                 (method m () (get this x))))))
 2)
 
+; Test seller, comprueba los llamados internos a metodos del mismo objeto
+(test (run-val
+       '(local
+          [(define seller (class
+                              (method unit () 1)
+                            (method price () (* (send this unit) 100))
+                            ))
+           (define S (new seller))]
+          (send S price))) 100)
+
+; Test de broker seller del apunte, comprueba los llamados a this
+(test (run-val
+       '(local
+          [(define seller (class
+                              (method unit () 1)
+                            (method price () (* (send this unit) 100))
+                            ))
+           (define broker (class <: seller
+                              (method unit () 2)))
+           (define B (new broker))
+           ]
+          (send B price)
+          )) 200)
+
+; Tests de crear dos instancias de una clase Point
+; Verificar que el set de p1 funciona
+(test (run-val '(local
+                  [(define Point (class (field x 0) (method move (n) (set this x (+ (get this x) n)))))
+                   (define p1 (new Point))
+                   (define p2 (new Point))]
+                  (seqn
+                   (send p1 move 10)
+                   (get p1 x)))) 10)
+
+; Verificar que el set de p1 no afecta la instancia p2
+(test (run-val '(local
+                  [(define Point (class (field x 0) (method move (n) (set this x (+ (get this x) n)))))
+                   (define p1 (new Point))
+                   (define p2 (new Point))]
+                  (seqn
+                   (send p1 move 10)
+                   (get p2 x)))) 0)
+
 ;; herencia simple
 
 (test (run-val '(local
@@ -74,6 +143,20 @@
                (define o (new c))]
               (send o f 20)))
 #f)
+
+;; Test de herencia de methodos,
+;; el objeto B parte buscando sus métodos desde su clase padre
+(test (run-val '(local
+                  [(define A (class (method foo () 0) (method bar () 1)))
+                   (define B (class <: A (method bar () 2)))
+                   (define b (new B))]
+                  (send b foo))) 0)
+
+(test (run-val '(local
+                  [(define A (class (method foo () 0) (method bar () 1)))
+                   (define B (class <: A (method bar () 2)))
+                   (define b (new B))]
+                  (send b bar))) 2)
 
 ;; soporte para super
 
@@ -88,8 +171,24 @@
           (send o g)))
 11)
 
+; Ejemplo del apunte, uso del super para definir métodos de clase hija
+; Point y colorPoint
+; También se verifica field shadowing
+
+(test (run-val '(local [(define Point (class (field x 1) (method number () (get this x))))
+                        (define ColorPoint (class <: Point (field y 2) (method number () (+ (get this y) (super number)))))
+                        (define cp (new ColorPoint))] (send cp number))) 3)
+
+; Ejemplo del apunte, uso del super para verificar que se realiza el lookup desde la clase padre de la clase de donde se encuentra el método
+(test (run-val '(local [(define A (class (method m () 4)))
+                        (define B (class <: A (method m () (+ 3 (super m)))))
+                        (define C (class <: B))
+                        (define c (new C))]
+                  (send c m))) 7)
+
 ;;field shadowing
 
+; field shadowing con get solamente
 (test (run-val '(local
               [(define A (class 
                            [field x 1]
@@ -102,3 +201,46 @@
               (send b ax)))
 1)
 
+; field shadowing con get y set, utilizando tambien dos fields con el mismo nombre del objeto
+(test (run-val '(local
+              [(define A (class 
+                           [field x 1]
+                           [field y 0]
+                           [method ax1 (val) (set this x val)]
+                           [method ax2 () (get this x)]))
+               (define B (class <: A
+                           [field x 2]
+                           [method bx () (get this x)]))
+               (define b (new B))]
+              (seqn (send b ax1 5)
+                    (+ (send b ax2) (send b bx))))) 7)
+
+
+
+;; lambdas with objects ;;
+
+;identity lambda
+(test (run-val '(local
+              [(define f (fun (x) x))]
+                  (f 1234))) 1234)
+;double a number
+(test (run-val '(local
+              [(define f (fun (x) (+ x x)))]
+                  (f 5))) 10)
+;lambda that uses a value of env 'l'
+(test (run-val '(local
+                  [(define l 0)
+                   (define s (fun () l))]
+                  (+ (s) 0))) 0)
+;addition lambda
+(test (run-val '(local
+                  [(define f (fun (a b) (+ a b)))]
+                  (f 2 3))) 5)
+;composition of lambdas
+(test (run-val '(local
+                  [(define f (fun (a b) (+ a b)))
+                   (define g (fun (a b c) (+ a (f b c))))]
+                  (g 2 3 5))) 10)
+;lambdas as object values (first order)
+(test (run-val '(local [(define f (fun () 0))]
+                  f)) "<Object>")
